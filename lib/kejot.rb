de443c72612cf6538446696f72e6aba55c4647e7
Reyza Permana Saputra <reyza.permana@wgs.co.id>
2019-08-20 15:17:34 +0700

    initial commit

---
+++ lib/kejot.rb
@@ -0,0 +1,15 @@
+require_relative "kejot/version.rb"
+
+module Kejot
+  SPELL_SET1 = %w(accurate sufficient ugly useful immediate entire healthy hot efficient dramatic)
+  SPELL_SET2 = %w(punch kick attack blow smash strike smack cut poke stab)
+  class Generator
+    def self.generate
+      new.generate
+    end
+
+    def generate
+      "#{SPELL_SET1[rand(0..9)]} #{SPELL_SET2[rand(0..9)]}"
+    end
+  end
+end
---
+++ lib/kejot/version.rb
@@ -0,0 +1,3 @@
+module Kejot
+  VERSION = "0.1.0"
+end
