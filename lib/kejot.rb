b795904d4bcfb1a94dbc9bd889be9299ea7e7031
Reyza Permana Saputra <reyza.permana@wgs.co.id>
2019-08-20 17:06:36 +0700

    add function

--- lib/kejot.rb
+++ lib/kejot.rb
@@ -3,6 +3,30 @@ require_relative "kejot/version.rb"
 module Kejot
   SPELL_SET1 = %w(accurate sufficient ugly useful immediate entire healthy hot efficient dramatic)
   SPELL_SET2 = %w(punch kick attack blow smash strike smack cut poke stab)
+
+  class Hola
+    def self.hi(language = "english")
+      translator = Translator.new(language)
+      translator.hi
+    end
+  end
+
+  class Hola::Translator
+    def initialize(language)
+      @language = language
+    end
+
+    def hi
+      case @language
+      when "spanish"
+        "hola mundo"
+      else
+        "hello world"
+      end
+    end
+  end
+
+
   class Generator
     def self.generate
       new.generate
